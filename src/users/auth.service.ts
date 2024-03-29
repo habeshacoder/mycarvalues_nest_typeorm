ac911a0f6be0d407482ba59bf9d10eea97caa06b
adoni <adoni@gmail.com>
2024-03-29 20:25:27 +0300

    hash and salting password

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,9 @@
 import { BadRequestException } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { randomBytes, scrypt as _scrypt, Hash } from 'crypto';
+import { promisify } from 'util';
 
+const scrypt = promisify(_scrypt);
 export class AuthService {
   constructor(private userService: UsersService) {}
   async signUp(email: string, password: string) {
@@ -8,6 +11,12 @@ export class AuthService {
     if (users.length) {
       return new BadRequestException('user already registered');
     }
+    // salt
+    const salt = randomBytes(8).toString('hex');
+    //hash password and salt
+    const hash = await scrypt(password, salt, 32);
+    //combine hash and salt
+    const result = salt + '.' + hash.toString();
   }
   signin() {}
 }

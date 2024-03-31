ae0569107ca9c26ffeb94bae460feed0ded953b9
adoni <adoni@gmail.com>
2024-04-01 00:17:59 +0300

    global interceptor

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,10 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersController } from './users.controller';
 import { AuthService } from './auth.service';
 import { CurrentUserInterceptors } from './interceptors/current-user.interceptor';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UsersService, AuthService, CurrentUserInterceptors],
+  providers: [
+    UsersService,
+    AuthService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CurrentUserInterceptors,
+    },
+  ],
   controllers: [UsersController],
 })
 export class UsersModule {}
